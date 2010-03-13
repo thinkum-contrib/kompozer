f3f00e36e6bc1dd7c49763793a596dce1d27c55e
Fabien Cazenave <kaze@kompozer.net>
2010-03-13 19:28:36 +0100

    Ukrainian has been added to release.py.

--- release.py
+++ release.py
@@ -33,8 +33,10 @@ import os
 import sys
 
 VERSION = "0.8b3"
-LOCALES = ["ca", "cs", "da", "de", "en-US", "eo", "es-ES", "fi", "fr", "hu", \
-           "hsb", "it", "ja", "nl", "pl", "pt-PT", "ru", "sl", "zh-CN", "zh-TW"]
+LOCALES = [ \
+  "ca", "cs", "da", "de", "en-US", "eo", "es-ES", "fi", "fr", "hu", "hsb", \
+  "it", "ja", "nl", "pl", "pt-PT", "ru", "sl", "uk", "zh-CN", "zh-TW"      \
+]
 
 
 ###############################################################################
@@ -216,7 +218,7 @@ def makeBinary(srcDir, platform, locale):
   elif (locale == "zh-CN" or locale == "zh-TW" or locale == "hsb" or locale == "eo"):
     replaceInFile(acceptCharsets, "utf-8,\*,iso-8859-1",   "all.js")
     replaceInFile(customCharset,  "UTF8",                  "editor.js")
-  elif (locale == "cs" or locale == "hu" or locale == "pl" or locale == "sl"):
+  elif (locale == "cs" or locale == "hu" or locale == "pl" or locale == "sl" or locale == "uk"):
     replaceInFile(acceptCharsets, "iso-8859-2,\*,utf-8",   "all.js")
     replaceInFile(customCharset,  "ISO-8859-2",            "editor.js")
   elif (locale == "ru"):
@@ -310,6 +312,7 @@ def makeInnoSetup(srcDir, locale):
     'ru'   : 'Russian',             \
     'sk'   : 'Slovak',              \
     'sl'   : 'Slovenian',           \
+    'uk'   : 'Ukrainian',           \
     'tr'   : 'Turkish',             \
     'zh-CN': 'ChineseSimplified',   \
     'zh-TW': 'ChineseTraditional'   \
